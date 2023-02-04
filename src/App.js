39e0e9e386a0d962e2d8a0dd93d6a6dcb54ebd99
brandmch <101467867+brandmch@users.noreply.github.com>
2023-02-04 15:34:33 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
 
 function App() {
+  console.log("swagola");
+
+  // fetch("https://api.monday.com/v2", {
+  //   method: "post",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     Authorization:
+  //       "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzNDU5OTMwOSwidWlkIjozOTE4NDY4MiwiaWFkIjoiMjAyMy0wMi0wNFQyMDowMjowOS44MjRaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTUwOTE3NzUsInJnbiI6InVzZTEifQ.9vew5bVPxWTRi6sNWTYp0lD7cT7PPGwcmb2HfH3fne8",
+  //   },
+  //   body: JSON.stringify({
+  //     query: "{me {name}}",
+  //   }),
+  // })
+  //   .then((res) => res.json())
+  //   .then((data) => console.log(data));
+
+  const GET_STUFF = gql`
+    query {
+      me {
+        name
+      }
+
+      boards(limit: 1) {
+        name
+
+        columns {
+          title
+          id
+          type
+        }
+
+        groups {
+          title
+          id
+        }
+
+        items {
+          name
+          group {
+            id
+          }
+
+          column_values {
+            id
+            value
+            text
+          }
+        }
+      }
+    }
+  `;
+
+  const { loading, error, data } = useQuery(GET_STUFF);
+  if (loading) {
+    console.log("loading");
+  }
+  if (error) {
+    console.log(error);
+  }
+  if (data) {
+    console.log(data);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const httpLink = createHttpLink({
+  uri: "https://api.monday.com/v2",
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: process.env.REACT_APP_MONDAY_AUTH,
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
